a20c13d59b09dc17c20d3172d7abbc7c3c6826a3
QR <1826255833@qq.com>
2022-05-12 20:22:29 +0800

    aop的补充

--- src/main/java/qr/program/spring/aop/package-info.java
+++ src/main/java/qr/program/spring/aop/package-info.java
@@ -1,5 +1,10 @@
 /**
  * 面向切面编程：可以将公共部分的代码进行提取通过面向切面技术插入到业务代码中的各个流程部分执行
+ * SpringAop 的使用方式：
+ * - 引入 aop 包
+ * - 创建一个 aspect 加上 @Aspect 和 @Component 注解
+ * - 根据需求定义 @Before @After *@Around 方法
+ * - 结合需求可以定义 pointCut 或者自定义注解实现切入
  * @Author qr
  * @Date 2022/5/12-17:11
  */
